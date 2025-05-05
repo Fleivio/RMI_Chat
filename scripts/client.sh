5790f4902b7efb9277b92de9d1022fc6cb64f5c7
flavio <flavioborinjunior@gmail.com>
2025-05-05 15:27:03 -0300

    policy adicionado

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,1 +1,5 @@
-java -cp build/jars/UserChat.jar:build/jars/ServerChat.jar client.UserChat $1
+
+java -cp build/classes \
+     client.UserChat $1
+    #  -Djava.rmi.server.hostname=10.1.1.230 \
+    #  -Djava.security.policy=rmi.policy \
--- scripts/server.sh
+++ scripts/server.sh
@@ -1,1 +1,6 @@
-java -cp build/jars/ServerChat.jar:build/jars/UserChat.jar server.ServerChat
+# java -cp build/jars/ServerChat.jar:build/jars/UserChat.jar server.ServerChat
+
+java -cp build/classes \
+     -Djava.rmi.server.hostname=10.1.1.230 \
+     -Djava.security.policy=rmi.policy \
+     server.ServerChat
